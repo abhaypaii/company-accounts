ad3533970f01763cb1d5a9749b5f2b7f257bb200
Abhay Pai <abhaypai1230@gmail.com>
2024-10-09 09:52:52 -0400

    Added dynamic variable for year columns instead of hard coding

--- Pages/1_Company Financials.py
+++ Pages/1_Company Financials.py
@@ -74,25 +74,27 @@ with c2:
 if not st.session_state.disable:
     financials, cashflow, balancesheet = get_reports(display_ticker)
     tab1, tab2, tab3 = st.tabs(['Financials', 'Cash Flow', 'Balance Sheet'])
+    year1 = financials.columns[0]
+    year0 = financials.columns[1]
     with tab1:
         try:
             financials.index = financials.index.str.strip()
             cols = st.columns(5)
             with cols[0]:
-                st.metric("EBITDA", value = "$"+millify(financials.loc["Normalized EBITDA", '2023'], precision = 2), 
-                        delta = millify(financials.loc["Normalized EBITDA", '2023'] - financials.loc["Normalized EBITDA", '2022'], precision = 2))
+                st.metric("EBITDA", value = "$"+millify(financials.loc["Normalized EBITDA", year1], precision = 2), 
+                        delta = millify(financials.loc["Normalized EBITDA", year1] - financials.loc["Normalized EBITDA", year0], precision = 2))
             with cols[1]:
-                st.metric("Net Income", value = "$"+millify(financials.loc["Net Income", '2023'], precision = 2), 
-                        delta = millify(financials.loc["Net Income", '2023'] - financials.loc["Net Income", '2022'], precision = 2))
+                st.metric("Net Income", value = "$"+millify(financials.loc["Net Income", year1], precision = 2), 
+                        delta = millify(financials.loc["Net Income", year1] - financials.loc["Net Income", year0], precision = 2))
             with cols[2]:
-                st.metric("Total Revenue", value = "$"+millify(financials.loc["Total Revenue", '2023'], precision = 2), 
-                        delta = millify(financials.loc["Total Revenue", '2023'] - financials.loc["Total Revenue", '2022'], precision = 2))
+                st.metric("Total Revenue", value = "$"+millify(financials.loc["Total Revenue", year1], precision = 2), 
+                        delta = millify(financials.loc["Total Revenue", year1] - financials.loc["Total Revenue", year0], precision = 2))
             with cols[3]:
-                st.metric("Earnings Per Share", value = "$"+millify(financials.loc["Diluted EPS", '2023'], precision = 2), 
-                        delta = millify(financials.loc["Diluted EPS", '2023'] - financials.loc["Diluted EPS", '2022'], precision = 2))
+                st.metric("Earnings Per Share", value = "$"+millify(financials.loc["Diluted EPS", year1], precision = 2), 
+                        delta = millify(financials.loc["Diluted EPS", year1] - financials.loc["Diluted EPS", year0], precision = 2))
             with cols[4]:
-                roe1 = financials.loc["Net Income", '2023']/balancesheet.loc["Stockholders Equity", '2023']
-                roe0 = financials.loc["Net Income", '2022']/balancesheet.loc["Stockholders Equity", '2022']
+                roe1 = financials.loc["Net Income", year1]/balancesheet.loc["Stockholders Equity", year1]
+                roe0 = financials.loc["Net Income", year0]/balancesheet.loc["Stockholders Equity", year0]
                 st.metric("Return on Equity", value = millify(roe1, precision = 2), 
                         delta = millify(roe1-roe0, precision = 2))
             with st.container(height=500):
@@ -104,20 +106,20 @@ if not st.session_state.disable:
         try:
             cols = st.columns(5)
             with cols[0]:
-                fcf1 = cashflow.loc['Free Cash Flow', '2023']
-                fcf0 = cashflow.loc['Free Cash Flow', '2022']
+                fcf1 = cashflow.loc['Free Cash Flow', year1]
+                fcf0 = cashflow.loc['Free Cash Flow', year0]
                 st.metric("Free Cash Flow", value = "$"+millify(fcf1, precision=2), delta = millify(fcf1-fcf0, precision=2) )
             with cols[1]:
-                ocf1 = cashflow.loc['Operating Cash Flow', '2023']
-                ocf0 = cashflow.loc['Operating Cash Flow', '2022']
+                ocf1 = cashflow.loc['Operating Cash Flow', year1]
+                ocf0 = cashflow.loc['Operating Cash Flow', year0]
                 st.metric("Operating Cash Flow", value = "$"+millify(ocf1, precision=2), delta = millify(ocf1-ocf0, precision=2) )
             with cols[2]:
-                icf1 = cashflow.loc['Investing Cash Flow', '2023']
-                icf0 = cashflow.loc['Investing Cash Flow', '2022']
+                icf1 = cashflow.loc['Investing Cash Flow', year1]
+                icf0 = cashflow.loc['Investing Cash Flow', year0]
                 st.metric("Investing Cash Flow", value = "$"+millify(icf1, precision=2), delta = millify(icf1-icf0, precision=2) )
             with cols[3]:
-                fincf1 = cashflow.loc['Financing Cash Flow', '2023']
-                fincf0 = cashflow.loc['Financing Cash Flow', '2022']
+                fincf1 = cashflow.loc['Financing Cash Flow', year1]
+                fincf0 = cashflow.loc['Financing Cash Flow', year0]
                 st.metric("Financing Cash Flow", value = "$"+millify(fincf1, precision=2), delta = millify(fincf1-fincf0, precision=2) )
             with cols[4]:
                 ncf1 = ocf1 + icf1 + fincf1
@@ -132,20 +134,20 @@ if not st.session_state.disable:
             try:
                 cols = st.columns(6)
                 with cols[0]:
-                    se1 = balancesheet.loc["Stockholders Equity", "2023"]
-                    se0 = balancesheet.loc["Stockholders Equity", "2022"]
-                    st.metric("Stockholders Equity", value="$"+millify(balancesheet.loc["Stockholders Equity", "2023"], precision = 2), 
-                            delta = millify(balancesheet.loc["Stockholders Equity", "2023"] - balancesheet.loc["Stockholders Equity", "2022"]))
+                    se1 = balancesheet.loc["Stockholders Equity", year1]
+                    se0 = balancesheet.loc["Stockholders Equity", year0]
+                    st.metric("Stockholders Equity", value="$"+millify(balancesheet.loc["Stockholders Equity", year1], precision = 2), 
+                            delta = millify(balancesheet.loc["Stockholders Equity", year1] - balancesheet.loc["Stockholders Equity", year0]))
                 with cols[1]:
-                    st.metric("Retained Earnings", value="$"+millify(balancesheet.loc["Retained Earnings", "2023"], precision = 2), 
-                            delta = millify(balancesheet.loc["Retained Earnings", "2023"] - balancesheet.loc["Retained Earnings", "2022"]))
+                    st.metric("Retained Earnings", value="$"+millify(balancesheet.loc["Retained Earnings", year1], precision = 2), 
+                            delta = millify(balancesheet.loc["Retained Earnings", year1] - balancesheet.loc["Retained Earnings", year0]))
                 with cols[2]:
-                    ta1 = balancesheet.loc["Total Assets", "2023"]
-                    ta0 = balancesheet.loc["Total Assets", "2022"]
+                    ta1 = balancesheet.loc["Total Assets", year1]
+                    ta0 = balancesheet.loc["Total Assets", year0]
                     tl1 = ta1-se1
                     tl0 = ta0-se0
-                    st.metric("Total Assets", value="$"+millify(balancesheet.loc["Total Assets", "2023"], precision = 2), 
-                            delta = millify(balancesheet.loc["Total Assets", "2023"] - balancesheet.loc["Total Assets", "2022"]))
+                    st.metric("Total Assets", value="$"+millify(balancesheet.loc["Total Assets", year1], precision = 2), 
+                            delta = millify(balancesheet.loc["Total Assets", year1] - balancesheet.loc["Total Assets", year0]))
                 with cols[3]:
                     st.metric("Total Liabilities", value="$"+millify(tl1, precision = 2), 
                             delta = millify(tl1 - tl0, precision=2))
@@ -154,8 +156,8 @@ if not st.session_state.disable:
                     st.metric("Debt-to-Equity Ratio", value= millify(tl1/se1, precision = 2), 
                             delta = millify(tl1/se1 - tl0/se0, precision=2))
                 with cols[5]:
-                    ca1, cl1 = balancesheet.loc['Current Assets', '2023'], balancesheet.loc['Current Liabilities', '2023']
-                    ca0, cl0 = balancesheet.loc['Current Assets', '2022'], balancesheet.loc['Current Liabilities', '2022']
+                    ca1, cl1 = balancesheet.loc['Current Assets', year1], balancesheet.loc['Current Liabilities', year1]
+                    ca0, cl0 = balancesheet.loc['Current Assets', year0], balancesheet.loc['Current Liabilities', year0]
                     st.metric("Current Ratio", value = millify(ca1/cl1, precision=2), delta = millify(ca1/cl1 - ca0/cl0, precision=2))
 
                 with st.container(height=500):
